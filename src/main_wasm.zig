3dba0d3280474f3188eed1f71dae800f4d0a9e73
last_arg <last_arg@tutanota.com>
2024-11-08 15:14:55 +0200

    html: step button

--- src/main_wasm.zig
+++ src/main_wasm.zig
@@ -12,13 +12,19 @@ fn logStr(str: []const u8) void {
     zjb.global("console").call("log", .{handle}, void);
 }
 
+var global_gol: *GameOfLife = undefined;
+var global_canvas_context: *zjb.Handle = undefined;
+const cell_size = 30; 
+const cell_size_i32: i32 = @intCast(cell_size); 
+
 pub const panic = zjb.panic;
 export fn main() void {
     zjb.global("console").call("log", .{zjb.constString("Hello from Zig")}, void);
 
-    const cell_size = 30; 
-    const cell_size_i32: i32 = @intCast(cell_size); 
-    var gol = GameOfLife.init(alloc, 7, 7) catch |e| zjb.throwError(e);
+    var gol = alloc.create(GameOfLife) catch unreachable;
+    gol.* = GameOfLife.init(alloc, 7, 7) catch |e| zjb.throwError(e);
+    // defer gol.deinit();
+    global_gol = gol;
 
     var glider = [_][]const u8{
         &.{0, 1, 0},
@@ -45,150 +51,60 @@ export fn main() void {
         log(obj);
     }
 
-    logStr("\n============================= html canvas example =============================");
-    {
-        const canvas = zjb.global("document").call("getElementById", .{zjb.constString("canvas")}, zjb.Handle);
-        defer canvas.release();
+    const canvas = zjb.global("document").call("getElementById", .{zjb.constString("canvas")}, zjb.Handle);
+    defer canvas.release();
 
-        const canvas_width: i32 = @intCast(cell_size * gol.width);
-        const canvas_height: i32 = @intCast(cell_size * gol.height);
-        canvas.set("width", canvas_width);
-        canvas.set("height", canvas_height);
+    const canvas_width: i32 = @intCast(cell_size * gol.width);
+    const canvas_height: i32 = @intCast(cell_size * gol.height);
+    canvas.set("width", canvas_width);
+    canvas.set("height", canvas_height);
 
-        const context = canvas.call("getContext", .{zjb.constString("2d")}, zjb.Handle);
-        defer context.release();
+    var context = canvas.call("getContext", .{zjb.constString("2d")}, zjb.Handle);
+    global_canvas_context = &context;
+    // defer context.release();
+    canvas_render(&context, gol);
 
-        const live_color = zjb.constString("#16191d"); 
-        const dead_color = zjb.constString("#f8f9fa"); 
+    const button_next_step = zjb.global("document").call("getElementById", .{zjb.constString("canvas-next-step")}, zjb.Handle);
+    defer button_next_step.release();
+    button_next_step.call("addEventListener", .{ zjb.constString("click"), zjb.fnHandle("clickCallback", clickCallback) }, void);
+}
 
-        context.set("fillStyle", dead_color);
-        const canvas_start: i32 = 0;
-        const canvas_end: i32 = 0;
-        context.call("fillRect", .{ canvas_start, canvas_end, canvas_width, canvas_height }, void);
+fn clickCallback(event: zjb.Handle) callconv(.C) void {
+    zjb.global("console").call("log", .{ zjb.constString("From keydown callback, event:"), event }, void);
+    defer event.release();
 
-        context.set("fillStyle", live_color);
+    global_gol.step() catch |e| {
+        logStr("GameOfLife.step failed");
+        zjb.throwError(e);
+    };
+    canvas_render(global_canvas_context, global_gol);
+}
 
-        for (0..gol.height) |row_index| {
-            const row_px: i32 = @intCast(row_index * cell_size);
-            for (0..gol.width) |col_index| {
-                const col_val = gol.grid.items[row_index * gol.width + col_index];
-                if (col_val == 1) {
-                    const col_px: i32 = @intCast(col_index * cell_size);
-                    context.call("fillRect", .{ col_px, row_px, cell_size_i32, cell_size_i32 }, void);
-                }
+fn canvas_render(context: *const zjb.Handle, gol: *const GameOfLife) void {
+    const canvas_width: i32 = @intCast(cell_size * gol.width);
+    const canvas_height: i32 = @intCast(cell_size * gol.height);
+
+    const live_color = zjb.constString("#16191d"); 
+    const dead_color = zjb.constString("#f8f9fa"); 
+
+    context.set("fillStyle", dead_color);
+    const canvas_start: i32 = 0;
+    const canvas_end: i32 = 0;
+    context.call("fillRect", .{ canvas_start, canvas_end, canvas_width, canvas_height }, void);
+
+    context.set("fillStyle", live_color);
+
+    for (0..gol.height) |row_index| {
+        const row_px: i32 = @intCast(row_index * cell_size);
+        for (0..gol.width) |col_index| {
+            const col_val = gol.grid.items[row_index * gol.width + col_index];
+            if (col_val == 1) {
+                const col_px: i32 = @intCast(col_index * cell_size);
+                context.call("fillRect", .{ col_px, row_px, cell_size_i32, cell_size_i32 }, void);
             }
         }
     }
-    
-    // {
-    //     const formatted = std.fmt.allocPrint(alloc, "Runtime string: current timestamp {d}", .{zjb.global("Date").call("now", .{}, f32)}) catch |e| zjb.throwError(e);
-    //     defer alloc.free(formatted);
-
-    //     const str = zjb.string(formatted);
-    //     defer str.release();
-
-    //     zjb.global("console").call("log", .{str}, void);
-    // }
-
-    // logStr("\n============================= Array View Example =============================");
-    // {
-    //     var arr = [_]u8{ 1, 2, 3 };
-    //     const obj = zjb.u8ArrayView(&arr);
-    //     defer obj.release();
-
-    //     logStr("View of Zig u8 array from Javascript, with its length");
-    //     log(obj);
-    //     log(obj.get("length", f64)); // 3
-
-    //     arr[0] = 4;
-    //     logStr("Changes from Zig are visible in Javascript");
-    //     log(obj);
-
-    //     obj.indexSet(1, 5);
-    //     logStr("Changes from Javascript are visible in Zig");
-    //     log(obj);
-
-    //     logStr("Unless wasm's memory grows, which causes the ArrayView to be invalidated.");
-    //     _ = @wasmMemoryGrow(0, 1);
-    //     arr[0] = 5;
-    //     log(obj);
-    //     log(obj.get("length", f64)); // 0
-    // }
-
-    // logStr("\n============================= Data View Examples =============================");
-    // logStr("dataView allows extraction of numbers from WASM's memory.");
-    // {
-    //     const arr = [_]u16{ 1, 2, 3 };
-    //     const obj = zjb.dataView(&arr);
-    //     defer obj.release();
-
-    //     logStr("dataView works for arrays.");
-    //     log(obj);
-    //     log(obj.call("getUint16", .{ @sizeOf(u16) * 0, true }, f32));
-    //     log(obj.call("getUint16", .{ @sizeOf(u16) * 1, true }, f32));
-    //     log(obj.call("getUint16", .{ @sizeOf(u16) * 2, true }, f32));
-    // }
-
-    // {
-    //     const S = extern struct {
-    //         a: u16,
-    //         b: u16,
-    //         c: u32,
-    //     };
-    //     const s = S{ .a = 1, .b = 2, .c = 3 };
-    //     const obj = zjb.dataView(&s);
-    //     defer obj.release();
-
-    //     logStr("dataView also works for structs, make sure they're extern!");
-    //     log(obj);
-    //     log(obj.call("getUint16", .{ @offsetOf(S, "a"), true }, f32));
-    //     log(obj.call("getUint16", .{ @offsetOf(S, "b"), true }, f32));
-    //     log(obj.call("getUint32", .{ @offsetOf(S, "c"), true }, f32));
-    // }
-
-    // logStr("\n============================= Maps and index getting/setting =============================");
-    // {
-    //     const obj = zjb.global("Map").new(.{});
-    //     defer obj.release();
-
-    //     const myI32: i32 = 0;
-    //     obj.indexSet(myI32, 0);
-    //     const myI64: i64 = 0;
-    //     obj.indexSet(myI64, 1);
-
-    //     obj.set("Hello", obj.indexGet(myI64, f64));
-
-    //     const str = zjb.string("some_key");
-    //     defer str.release();
-    //     obj.indexSet(str, 2);
-
-    //     log(obj);
-    // }
-
-
-    // logStr("\n============================= Exporting functions (press a key for a callback) =============================");
-    // zjb.global("document").call("addEventListener", .{ zjb.constString("keydown"), zjb.fnHandle("keydownCallback", keydownCallback) }, void);
-
-    // logStr("\n============================= Handle vs ConstHandle =============================");
-    // {
-    //     logStr("zjb.global and zjb.constString add their ConstHandle on first use, and remember for subsiquent uses.  They can't be released.");
-    //     logStr("While zjb.string and Handle return values must be released after being used or they'll leak.");
-    //     logStr("See that some string remain in handles, while others have been removed after use.");
-    //     const handles = zjb.global("zjb").get("_handles", zjb.Handle);
-    //     defer handles.release();
-    //     log(handles);
-    // }
-
-    // logStr("\n============================= Testing for unreleased handles =============================");
-    // logStr("\nIt's good to do this often.  Assert that the count is <= the number of handles you'll keep stored in long term state.");
-    // std.debug.assert(zjb.unreleasedHandleCount() == 0);
 }
-
-// fn keydownCallback(event: zjb.Handle) callconv(.C) void {
-//     defer event.release();
-
-//     zjb.global("console").call("log", .{ zjb.constString("From keydown callback, event:"), event }, void);
-// }
 
 // var value: i32 = 0;
 // fn incrementAndGet(increment: i32) callconv(.C) i32 {
